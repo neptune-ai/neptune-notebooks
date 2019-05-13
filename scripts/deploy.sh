fc67c4fe435c9fb8da4efba55f745860ec3ee95c
Łukasz Walkiewicz <lukasz.walkiewicz@codilime.com>
2019-05-13 12:48:12 +0200

    Set initial version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$TRAVIS_PYTHON_VERSION" == "3.6" ]]
 then
     # pypi package
-    twine upload --repository-url https://test.pypi.org/legacy/ dist/*.tar.gz
+    twine upload dist/*.tar.gz
 
     # npm package
     cd packages/labextension
