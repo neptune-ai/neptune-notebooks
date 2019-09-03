df3621017ba9f7dcb2029a4dab837b7624f339e3
Michał Kozielski <michal.kozielski@neptune.ml>
2019-09-03 14:18:55 +0200

    Implement platform metadata and api calls.

--- packages/neptune-extension/src/common/components/App.js
+++ packages/neptune-extension/src/common/components/App.js
@@ -3,8 +3,36 @@ import React from 'react';
 import ToolbarWrapper from 'platform/components/ToolbarWrapper';
 import ToolbarButton from 'platform/components/ToolbarButton';
 
-const App = () => {
-  const [clicked, setClicked] = React.useState(false);
+import {
+  getAccessToken,
+  createNotebook,
+  createCheckpoint,
+} from 'common/api/backend';
+
+const App = ({
+  platformNotebook,
+}) => {
+
+  const [ metadata, setMetadata ] = React.useState(platformNotebook.getMetadata);
+
+  const handleConfigure = () => {
+    getAccessToken()
+      .then(() => Promise.all([ createNotebook(), platformNotebook.getContent() ]))
+      .then(([ { data: neptuneData }, notebookData ]) => {
+        createCheckpoint(neptuneData.id, notebookData, metadata.path)
+          /* metadata changed... */
+          .then(() => platformNotebook.saveNotebookId(neptuneData.id))
+          .then(() => setMetadata(platformNotebook.getMetadata()));
+      });
+  };
+
+  const handleUpload = () => {
+    getAccessToken()
+      .then(() => platformNotebook.getContent())
+      .then((notebookData) => createCheckpoint(metadata.notebookId, notebookData, metadata.path));
+  };
+
+  const initialized = !!window.localStorage.getItem('neptune_api_token') && !!metadata.notebookId;
 
   return (
     <div id="neptune-app">
@@ -13,18 +41,15 @@ const App = () => {
           label={'Configure'}
           title="Connect to Neptune"
           icon="neptune"
-          compact={clicked}
-          onClick={() => {
-            setClicked(true);
-            console.log('neptune button clicked.');
-          }}
+          compact={initialized}
+          onClick={handleConfigure}
         />
         <ToolbarButton
           label="Upload"
           title="Upload to Neptune"
           icon="fa-cloud-upload"
-          visible={clicked}
-          onClick={() => console.log('upload button clicked.')}
+          visible={initialized}
+          onClick={handleUpload}
         />
       </ToolbarWrapper>
     </div>
