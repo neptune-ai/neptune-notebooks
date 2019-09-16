8ce13442124ffaf229acce3ee71ffc0fcb286ac1
Marcin Kierski <marcin.kierski@neptune.ml>
2019-09-16 17:38:54 +0200

    Allow redux dev tools only in development mode

---
+++ packages/neptune-extension/src/types/global.d.ts
@@ -0,0 +1,3 @@
+// These are globally available variables injected by our webpack config
+declare const PLATFORM: 'labextension' | 'nbextension';
+declare const APP_ENV: 'production' | 'development';
