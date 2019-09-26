96598a5b88f409ff3ce948383940bf6e1e40ff6e
Maciej Stankiewicz <tawezik@gmail.com>
2019-09-26 11:49:06 +0200

    Add redux actions, selectors and reducer for notifications (#79)

---
+++ packages/neptune-extension/src/common/state/notifications/reducer.ts
@@ -0,0 +1,41 @@
+import {
+  NotificationsActions,
+  Notification,
+} from "./actions";
+
+interface NotificationsState {
+  entries: Notification[]
+}
+
+const initialState: NotificationsState = {
+  entries: []
+};
+
+export function notificationsReducer(
+  state: NotificationsState = initialState,
+  action: NotificationsActions,
+): NotificationsState {
+  const {
+    type,
+    payload,
+  } = action;
+
+  switch (type) {
+    case 'NOTIFICATION_ADD': {
+      return {
+        ...state,
+        entries: [...state.entries, payload as Notification],
+      }
+    }
+
+    case 'NOTIFICATION_REMOVE': {
+      return {
+        ...state,
+        entries: state.entries.filter((entry: Notification) => entry.id !== payload.id),
+      }
+    }
+
+    default:
+      return state;
+  }
+}
--- packages/neptune-extension/src/common/state/reducers.ts
+++ packages/neptune-extension/src/common/state/reducers.ts
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux';
 // Module
 import { notebookReducer as notebook } from './notebook/reducer';
 import { configurationReducer as configuration } from './configuration/reducer';
+import { notificationsReducer as notifications } from './notifications/reducer';
 
 const reducers = {
   configuration,
   notebook,
+  notifications,
 };
 
 const rootReducer = combineReducers(reducers);
