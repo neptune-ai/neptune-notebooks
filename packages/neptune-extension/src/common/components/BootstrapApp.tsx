2ec1dea89606a099491d5fef40cb2197eed74ecc
Michał Kozielski <michal.kozielski@neptune.ml>
2019-09-26 10:33:05 +0200

    Fix redux store configuration.

--- packages/neptune-extension/src/common/components/BootstrapApp.tsx
+++ packages/neptune-extension/src/common/components/BootstrapApp.tsx
@@ -4,12 +4,20 @@ import {Provider} from 'react-redux';
 import configureStore from 'common/state/store';
 import App, {AppProps} from './App';
 
-const store = configureStore();
-
 export const BootstrapApp: React.FC<AppProps> = ({
   platformNotebook
-}) => (
-  <Provider store={store}>
-    <App platformNotebook={platformNotebook} />
-  </Provider>
-);
+}) => {
+  const storeRef = React.useRef<ReturnType<typeof configureStore>>();
+  if (!storeRef.current) {
+    /*
+     * In JupyterLab every notebook is a separate application.
+     */
+    storeRef.current = configureStore();
+  }
+
+  return (
+    <Provider store={storeRef.current}>
+      <App platformNotebook={platformNotebook} />
+    </Provider>
+  );
+};
