9a5e651acf06e9d3078f20d4520c9b1b3476c089
Maciej Stankiewicz <tawezik@gmail.com>
2019-09-30 15:24:44 +0200

    Add Notification component

--- packages/neptune-extension/src/common/components/notifications/NotificationsContainer.tsx
+++ packages/neptune-extension/src/common/components/notifications/NotificationsContainer.tsx
@@ -1,43 +1,25 @@
 // Libs
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 // App
 import { getNotifications } from 'common/state/notifications/selectors';
-import { removeNotification } from 'common/state/notifications/actions';
-import Toast from 'common/components/toast/Toast';
 import NotificationsPortal from 'common/components/notifications/notifications-portal/NotificationsPortal';
-import {CheckpointSuccessfulNotification} from './renderers/CheckpointSuccessfulNotification';
-import {UpgradeAvailableNotification} from './renderers/UpgradeAvailableNotification';
+import Notification from 'common/components/notifications/notification/Notification';
 
 // Module
 const NotificationsContainer: React.FC = () => {
   const notifications = useSelector(getNotifications);
-  const dispatch = useDispatch();
-  const deleteNotification = (id: string) => dispatch(removeNotification(id));
 
   return (
     <NotificationsPortal>
       {
-        notifications.map((notification, key) => {
-          if (notification.type === 'checkpoint-successful') {
-            return <CheckpointSuccessfulNotification key={key} {...notification} onClose={() => (deleteNotification(notification.id))} />
-          } else if (notification.type === 'upgrade-available') {
-            return <UpgradeAvailableNotification key={key} onClose={() => (deleteNotification(notification.id))} />
-          }
-
-          else {
-            return (
-              <Toast
-                key={key}
-                onClose={() => (deleteNotification(notification.id))}
-                type="success"
-              >
-                {JSON.stringify(notification, null, 2)}
-              </Toast>
-            )
-          }
-        })
+        notifications.map((notification) => (
+          <Notification
+            key={notification.id}
+            {...notification}
+          />
+        ))
       }
     </NotificationsPortal>
   )
