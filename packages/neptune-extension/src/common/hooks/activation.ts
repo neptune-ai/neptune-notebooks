ad2311a90659e48bcf04679f3dba879008c2128c
Marcin Kierski <marcin.kierski@neptune.ml>
2019-10-01 16:28:10 +0200

    Show activation toast only when user activates manually

--- packages/neptune-extension/src/common/hooks/activation.ts
+++ packages/neptune-extension/src/common/hooks/activation.ts
@@ -1,18 +1,15 @@
 
 import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import {useSelector} from 'react-redux'
 
 import { PlatformNotebook } from 'types/platform'
 
 import { getConfigurationState } from 'common/state/configuration/selectors';
 import { getNotebookState } from 'common/state/notebook/selectors';
-import { addNotification } from 'common/state/notifications/actions';
 
 import { executeActivationCode } from 'common/utils/env';
 
 export function createActivationHandler(platformNotebook: PlatformNotebook) {
-  const dispatch = useDispatch();
-
   const {
     apiToken,
     isApiTokenValid,
@@ -25,10 +22,6 @@ export function createActivationHandler(platformNotebook: PlatformNotebook) {
   React.useEffect(() => {
     if (apiToken && isApiTokenValid) {
       executeActivationCode(platformNotebook, apiToken, notebook);
-      dispatch(addNotification({
-        type: "success",
-        data: "neptune-client configuration activated successfully",
-      }))
     }
   }, [apiToken, isApiTokenValid, notebook])
 }
