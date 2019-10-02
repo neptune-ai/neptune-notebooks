3c0f7980a6d4138b2a8fb977871165dff4d954b4
Marcin Ki <kierson@users.noreply.github.com>
2019-10-02 11:39:26 +0200

    Implement notifications fade in/out (#115)

--- packages/neptune-extension/src/common/components/notifications/NotificationsContainer.tsx
+++ packages/neptune-extension/src/common/components/notifications/NotificationsContainer.tsx
@@ -1,11 +1,13 @@
 // Libs
 import React from 'react';
 import { useSelector } from 'react-redux'
+import {TransitionGroup} from 'react-transition-group';
 
 // App
 import { getNotifications } from 'common/state/notifications/selectors';
 import NotificationsPortal from 'common/components/notifications/notifications-portal/NotificationsPortal';
 import Notification from 'common/components/notifications/notification/Notification';
+import {FadeTransition} from 'common/components/animation/FadeTransition';
 
 // Module
 const NotificationsContainer: React.FC = () => {
@@ -13,14 +15,15 @@ const NotificationsContainer: React.FC = () => {
 
   return (
     <NotificationsPortal>
-      {
-        notifications.map((notification) => (
-          <Notification
-            key={notification.id}
-            {...notification}
-          />
-        ))
-      }
+      <TransitionGroup component={null}>
+        {
+          notifications.map((notification) => (
+            <FadeTransition key={notification.id}>
+              <Notification {...notification} />
+            </FadeTransition>
+          ))
+        }
+      </TransitionGroup>
     </NotificationsPortal>
   )
 };
